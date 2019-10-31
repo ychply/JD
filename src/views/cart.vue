973b1168bb4223f8d64d8d3b94ffac80a902d3ae
ychply <1556891295@qq.com>
2019-10-31 18:30:11 +0800

    购物车

--- src/views/cart.vue
+++ src/views/cart.vue
@@ -11,8 +11,22 @@
 		</div>
 		<div class="head_wrap">
 			<span class="span1"><i class="icon iconchoose"></i></span>
-			<span class="span2"></span>
+			<span class="span2">京东自营</span>
+			<span class="span3">
+				<i class="icon icontishi"></i><i class="btn_inline">凑单免运费</i><i class="btn_inline">优惠券</i>
+			</span>
 		</div>
+		<ul class="cart_list">
+			<li>
+				<i class="icon icontishi"></i>
+				<div class="imgs">
+					<img src="" alt="" />
+				</div>
+				<div class="content">
+					
+				</div>
+			</li>
+		</ul>
 	</div>
 
 </template>
@@ -44,8 +58,91 @@
 </script>
 
 <style lang="scss" scoped="scoped">
+.cart_list{
+	width:100%;
+	li{
+		width:100%;
+		height: 270px;
+		display: flex;
+		justify-content: flex-start;
+		background-color: #fff;
+		.content{
+			flex: 1;
+			
+		}
+		.span1{
+			width:100px;
+			height: 100%;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.imgs{
+			width:200px;
+			height: 200px;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			img{
+				max-width: 100%;
+			}
+		}
+	}
+}
 .head_wrap{
-	
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	width:100%;
+	height: 80px;
+	.span1{
+		display: flex;
+		width:80px;
+		height: 100%;
+		justify-content: center;
+		align-items: center;
+		i{
+			font-size:44px;
+			color:#aaa;
+		}
+	}
+	.span2{
+		width:200px;
+		font-size: 32px;
+		color:#333;
+		font-weight: bold;
+		text-align: left;
+		padding-left:10px;
+		line-height: 1em;
+		letter-spacing: 2px;
+	}
+	.span3{
+		flex: 1;
+		text-align: right;
+		.icon{
+			font-size: 26px;
+			margin-right:8px;
+			color:#ccc;
+		}
+		.btn_inline{
+			font-size: 18px;
+			color:#f2270c;
+			&:last-child{
+				margin:0 15px 0 20px;
+				position: relative;
+				&:before{
+					content: "";
+					display: block;
+					width:2px;
+					height: 18px;
+					background-color:#ccc;
+					position: absolute;
+					left:-10px;
+					top:4px;
+				}
+			}
+		}
+	}
 }
 .mod_blockTips{
 	width:100%;
